b041278562a97988c74a48b21922eaa8153dfc58
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-10-23 08:10:20 +0100

    feat: Add cocktails section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import gsap from "gsap";
@@ -11,7 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
     </main>
   );
 };
