6fc31529a8838a4a7d00fa8191dc6539b1938ea1
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-10-23 07:51:33 +0100

    feat: Added video animation to the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,43 @@
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "react-responsive";
+import { useRef } from "react";
 
 const Hero = () => {
-  useGSAP(() => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  /**
+   * Sets the text animations for the hero section.
+   * Splits the hero title and subtitle text and animates each character
+   * and line with a staggered animation.
+   */
+  const setTextAnimations = () => {
+    // Split hero title text so each character is animated
     const heroSplit = new SplitText(".title", {
       type: "chars, words",
     });
+
+    // Split hero subtitle text so each line in the paragraph is animated
     const paragraphSplit = new SplitText(".subtitle", {
       type: "lines",
     });
 
+    // Add a text gradient to each character of the hero title
     heroSplit.chars.forEach((char) => {
       char.classList.add("text-gradient");
     });
+
+    // Animate each character of the hero title
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 1.8,
       ease: "expo.out",
       stagger: 0.05,
     });
+
+    // Animate each line of the hero subtitle
     gsap.from(paragraphSplit.lines, {
       opacity: 0,
       yPercent: 100,
@@ -28,6 +46,15 @@ const Hero = () => {
       stagger: 0.05,
       delay: 1,
     });
+  };
+
+  /**
+   * Animates the left and right leaves when scrolling.
+   * The animation is done by setting the y position of the leaves
+   * based on the scroll position of the hero section.
+   */
+  const setLeafAnimations = () => {
+    // Animate the left and right leaves when scrolling
     gsap
       .timeline({
         scrollTrigger: {
@@ -39,6 +66,44 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+  };
+
+  /**
+   * Sets the start and end values for the video based on mobile or desktop
+   * and creates a timeline for the video so it plays each frame when scrolling.
+   */
+  const setVideoAnimation = () => {
+    // Set the start and end values for the video
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    // Create a timeline for the video so it plays each frame when scrolling
+    const videoTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    if (videoRef.current)
+      /**
+       * Called when the video metadata has been loaded.
+       * Sets the to animation to the end of the video.
+       */
+      videoRef.current.onloadedmetadata = () => {
+        videoTimeline.to(videoRef.current, {
+          currentTime: videoRef.current?.duration,
+        });
+      };
+  };
+
+  useGSAP(() => {
+    setTextAnimations();
+    setLeafAnimations();
+    setVideoAnimation();
   }, []);
   return (
     <>
@@ -73,6 +138,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
